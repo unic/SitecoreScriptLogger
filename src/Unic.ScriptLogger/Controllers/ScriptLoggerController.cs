f30971691faad367dc6916daa3034c019536ac06
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2015-01-30 11:22:44 +0100

    Add Mvc controller for handling requests.

---
+++ src/Unic.ScriptLogger/Controllers/ScriptLoggerController.cs
@@ -0,0 +1,12 @@
+﻿namespace Unic.ScriptLogger.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ScriptLoggerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("this is the log", "text/plain");
+        }
+    }
+}
---
+++ src/Unic.ScriptLogger/Pipelines/Initialize/AddMvcRoute.cs
@@ -0,0 +1,19 @@
+﻿namespace Unic.ScriptLogger.Pipelines.Initialize
+{
+    using Sitecore.Pipelines;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public class AddMvcRoute
+    {
+        public void Process(PipelineArgs args)
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        private static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute("ScriptLoggerRoute", "scriptlogger/{action}", new { controller = "ScriptLogger", action = "Index" });
+        }
+    }
+}
